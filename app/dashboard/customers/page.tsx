c25d83e4e73ceccc14a0686283b4bb243c73cddb
Trenton Sadrakula <tsadrakula@gmail.com>
2023-11-16 23:56:24 -0600

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CustomersPage = () => {
+  return <p>Customers page</p>;
+};
+
+export default CustomersPage;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const InvoicesPage = () => {
+  return <p>Invoices Page</p>;
+};
+
+export default InvoicesPage;
